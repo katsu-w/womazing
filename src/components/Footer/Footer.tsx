7929e5f0b7e5999ad59dfb23eb86898549e8a43d
Александр Зайцев <sdjgsinhzoghaodg@yandex.ru>
2025-08-15 14:42:57 +0300

    decomposed navbar + refactor linked styles

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import './Footer.scss';
+import NavBar from "../UI/NavBar";
+
+export function Footer() {
+	
+	return (
+		<footer className="footer">
+			<div className="container">
+				<div className="footer-inner">
+					<NavBar parent='footer' />
+				</div>
+			</div>
+		</footer>
+	)
+}
---
+++ src/components/UI/NavBar/NavBar.tsx
@@ -0,0 +1,44 @@
+import './NavBar.scss';
+import {Link} from "react-router";
+
+interface NavBarProps {
+	parent: string;
+}
+
+export function NavBar(props: NavBarProps) {
+	const {parent} = props;
+	
+	return (
+		<nav className="nav header__nav">
+			<Link to="/" className="logo header__logo">
+				<img src="/logo.svg"
+				     alt="Womazing"
+				     width="139"
+				     height="25"
+				/>
+			</Link>
+			<ul className="nav__list header__nav__list">
+				<li className="nav__item header__nav__item active">
+					<Link to="/">
+						Главная
+					</Link>
+				</li>
+				<li className="nav__item header__nav-item">
+					<Link to="/shop">
+						Магазин
+					</Link>
+				</li>
+				<li className="nav__item header__nav-item">
+					<Link to="/about">
+						О бренде
+					</Link>
+				</li>
+				<li className="nav__item header__nav-item">
+					<Link to="/contacts">
+						Контакты
+					</Link>
+				</li>
+			</ul>
+		</nav>
+	)
+}
