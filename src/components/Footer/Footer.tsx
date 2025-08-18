38391361651167e3fa403e9d3160de1d793aba01
Александр Зайцев <sdjgsinhzoghaodg@yandex.ru>
2025-08-18 21:41:03 +0300

    finished footer (yay)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,16 +9,60 @@ export function Footer() {
 				<div className="footer-inner">
 					<NavBar parent="footer" />
 					<div className="footer__contacts">
-						<a href="tel:+74958235412"
-						   className="phone"
-						>
-							+7 (495) 823-54-12
-						</a>
-						<a href="mailto:hello@womazing.com"
-						   className="mail"
-						>
-							hello@womazing.com
-						</a>
+						<div className="footer__traditionals">
+							<a href="tel:+74958235412"
+							   className="phone"
+							>
+								+7 (495) 823-54-12
+							</a>
+							<a href="mailto:hello@womazing.com"
+							   className="mail"
+							>
+								hello@womazing.com
+							</a>
+						</div>
+						<div className="footer__social-media">
+							<a
+								class="footer__social-link"
+								href="https://github.com/katsu-w"
+							>
+								<img
+									src="/public/instagram-icon.svg"
+									alt=""
+									width="22"
+									height="22"
+								/>
+							</a>
+							<a
+								class="footer__social-link"
+								href="https://github.com/katsu-w"
+							>
+								<img
+									src="/public/facebook-icon.svg"
+									alt=""
+									width="21"
+									height="21"
+								/>
+							</a>
+							<a
+								class="footer__social-link"
+								href="https://github.com/katsu-w"
+							>
+								<img
+									src="/public/twitter-icon.svg"
+									alt=""
+									width="23"
+									height="23"
+								/>
+							</a>
+						</div>
+						<img
+							className="footer__payments"
+							src="/public/visa-mastercard-logos.png"
+							alt=""
+							width="68"
+							height="22"
+						/>
 					</div>
 				</div>
 			</div>
